b71674b9df65a61727d973497e9249a7c4eec3bb
lonycell <lonycell@gmail.com>
2022-03-21 23:23:44 +0900

    Initial Commit

---
+++ actio/actioApp.swift
@@ -0,0 +1,17 @@
+//
+//  actioApp.swift
+//  actio
+//
+//  Created by lonycell on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct actioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
